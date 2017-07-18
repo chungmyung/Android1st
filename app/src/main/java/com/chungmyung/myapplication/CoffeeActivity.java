c27088d76a82cf4831338410ac990344a40bd3df
JSKim <mkcjsk@gmail.com>
2017-07-18 13:21:34 +0900

    Framelayout gravity layout gravity & review

---
+++ app/src/main/java/com/chungmyung/myapplication/CoffeeActivity.java
@@ -0,0 +1,15 @@
+package com.chungmyung.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoffeeActivity extends AppCompatActivity {
+   // 메인
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 레이아웃 설정
+        setContentView(R.layout.activity_ooffee);
+    }
+}
