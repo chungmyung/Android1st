7a0a06838d4577e6a9b2f969a3ab1adfbbc3e44b
JSKim <mkcjsk@gmail.com>
2017-07-19 16:14:24 +0900

    Basketball 작업연습

---
+++ app/src/main/java/com/chungmyung/myapplication/BasketballActivity.java
@@ -0,0 +1,61 @@
+package com.chungmyung.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class BasketballActivity extends AppCompatActivity {
+
+    private TextView mScoreaTextView;
+    private TextView mScorebTextView;
+    private int mScorea = 0;
+    private int mScoreb = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketball);
+
+        mScoreaTextView = (TextView) findViewById(R.id.scorea_text);
+        mScoreaTextView.setText("" + mScorea);
+        mScorebTextView = (TextView) findViewById(R.id.scoreb_text);
+        mScorebTextView.setText("" + mScoreb);
+    }
+
+
+    public void point3aButtonClicked(View view) {
+        mScorea+=3;
+        mScoreaTextView.setText("" + mScorea);
+    }
+
+    public void point3bButtonClicked(View view) {
+        mScoreb+=3;
+        mScoreaTextView.setText("" + mScoreb);
+    }
+
+
+    public void point2aButtonClicked(View view) {
+        mScorea+=2;
+        mScoreaTextView.setText("" + mScorea);
+    }
+
+    public void point2bButtonClicked(View view) {
+        mScoreb+=2;
+        mScoreaTextView.setText("" + mScoreb);
+    }
+
+    public void pointa1buttonClicked(View view) {
+        mScorea+=1;
+        mScoreaTextView.setText("" + mScorea);
+    }
+
+    public void point1bButtonClicked(View view) {
+        mScoreb+=1;
+        mScoreaTextView.setText("" + mScoreb);
+    }
+
+    public void resetButtonClicked(View view) {
+    }
+}
--- app/src/main/java/com/chungmyung/myapplication/CoffeeActivity.java
+++ app/src/main/java/com/chungmyung/myapplication/CoffeeActivity.java
@@ -1,18 +1,23 @@
 package com.chungmyung.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.text.DecimalFormat;
 
-import java.text.NumberFormat;
 
 public class CoffeeActivity extends AppCompatActivity {
-   // 메인
+    public static final int MIN_Quantity = 1;
+    private TextView mQuantityTextView;
+    private TextView mPriceTextView;
+    private int mQuantity = 1;
+
+    //    private DecimalFormat mFormat =
+    private DecimalFormat mFormat = new DecimalFormat("#,##0.00");
+
+    // 메인
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,43 +25,31 @@ public class CoffeeActivity extends AppCompatActivity {
         // 레이아웃 설정
         setContentView(R.layout.activity_coffee);
         //xml에 있은 view의 레퍼런스를 가져오는 방법
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        mQuantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        mQuantityTextView.setText("" + mQuantity);
+//        mQuantityTextView.setText(String.valuaOf(mQuantity));
+        mPriceTextView = (TextView) findViewById(R.id.price_text_view);
+        mPriceTextView.setText("3000");
+        mQuantityTextView.setText("" + mQuantity);
 
-        quantityTextView.setText("10");
     }
 
-
-
-
-    public void submitOrder(View view){
-        int quantity = 0;
-        String priceMessage = "Price $" + (quantity * 5);
-        displayMessage(priceMessage);
-    }
-
-    private void displayPrice(int i) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + i);
-    }
-
-    private void display(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
 
     public void minusButtonClicked(View view) {
-        Toast.makeText(this, "잘눌림", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "잘눌림", Toast.LENGTH_SHORT).show();
+        mQuantity--;
+        if (mQuantity < MIN_Quantity) {
+            mQuantity = MIN_Quantity;
+        }
+        mQuantityTextView.setText("" + mQuantity);
+        mPriceTextView.setText(mFormat.format(mQuantity * 3000) + "원");
+
     }
 
     public void plusButtonClicked(View view) {
-        Toast.makeText(this, "잘눌림", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "잘눌림", Toast.LENGTH_SHORT).show();
+        mQuantity++;
+        mQuantityTextView.setText("" + mQuantity);
+        mPriceTextView.setText(mFormat.format(mQuantity * 3000) + "원");
     }
 }
