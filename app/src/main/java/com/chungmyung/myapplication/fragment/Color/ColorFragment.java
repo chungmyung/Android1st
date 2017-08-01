97034e39fbce0920edbf46a1822e4fe9a7e381d0
JSKim <mkcjsk@gmail.com>
2017-08-01 21:29:21 +0900

    factory method를 만들어서 동적 적용을 한 경우

--- app/src/main/java/com/chungmyung/myapplication/fragment/Color/ColorFragment.java
+++ app/src/main/java/com/chungmyung/myapplication/fragment/Color/ColorFragment.java
@@ -1,7 +1,9 @@
 package com.chungmyung.myapplication.fragment.Color;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,16 +11,43 @@ import android.view.ViewGroup;
 
 import com.chungmyung.myapplication.R;
 
+import java.util.Random;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ColorFragment extends Fragment {
 
-
+    // 프래그먼트는 반드시 빈 생성자 만 있어야 한다.!!!
     public ColorFragment() {
         // Required empty public constructor
     }
 
+    // Design pattern 중에서는 new Instance 생성하여 factory method를 만들어 주는 기능이 있다.
+    public static ColorFragment newInstance(int color) {
+
+        Bundle args = new Bundle();
+        args.putInt("color",color);
+
+        ColorFragment fragment = new ColorFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        int r = new Random().nextInt(256);
+        int g = new Random().nextInt(256);
+        int b = new Random().nextInt(256);
+        int color = Color.argb(255, r, g, b);
+
+        // Acitivity에서 넘긴 것을  받을땐 getArgument로 받는다.
+        color = getArguments().getInt("color");
+        view.setBackgroundColor(color);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,8 +55,6 @@ public class ColorFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_color, container, false);
     }
-
-
 
 
 }
---
+++ app/src/main/java/com/chungmyung/myapplication/fragment/Color/colerActivity.java
@@ -0,0 +1,60 @@
+package com.chungmyung.myapplication.fragment.Color;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.chungmyung.myapplication.R;
+
+public class colerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coler);
+    }
+
+
+
+    public void onCreateRedFragmentButtonClicked(View view) {
+        //새로운 프래그 먼트 생성
+        ColorFragment fragment = ColorFragment.newInstance(Color.RED);
+
+        //프래그먼트에게 넘겨주기위해선   Bundle에다 넘겨야 한다. 위의 coding으로 없어지는 부분
+//        Bundle bundle = new Bundle();
+//        bundle.putInt("color", Color.RED);
+//        bundle.putString("name", "어쩌구");
+//        fragment.setArguments(bundle);
+
+        // R.id.container영역에 프래그먼트를 교체하겠다.
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+
+
+
+    }
+
+    public void onCreateBlueFragmentButtonClicked(View view) {
+        //새로운 프래그 먼트 생성
+        ColorFragment fragment = ColorFragment.newInstance(Color.BLUE);
+
+        // R.id.container영역에 프래그먼트를 교체하겠다.
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    public void onCreateGreenFragmentButtonClicked(View view) {
+
+        //새로운 프래그 먼트 생성
+        ColorFragment fragment =ColorFragment.newInstance(Color.GREEN);
+
+        // R.id.container영역에 프래그먼트를 교체하겠다.
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+
+    }
+}
